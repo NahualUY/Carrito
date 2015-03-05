66d684ec36c737a467507ba80d7866729f023fcb
santiago cotto <taioloco@gmail.com>
2015-03-05 20:32:04 -0200

    barcoded products

---
+++ public/js/product.js
@@ -0,0 +1,6 @@
+﻿function product(sku, name, description, price) {
+    this.sku = sku; // product code (SKU = stock keeping unit)
+    this.name = name;
+    this.description = description;
+    this.price = price;
+}
